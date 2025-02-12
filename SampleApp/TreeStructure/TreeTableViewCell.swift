2ce301d149945fc0d940adaa9dac19badec01864
Ravindu Jayasekara <rav.t.jay97@gmail.com>
2025-02-12 22:25:40 +0530

    added tree structure and other apps

---
+++ SampleApp/TreeStructure/TreeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TreeTableViewCell.swift
+//  SampleApp
+//
+//  Created by ravindu jayasekara on 2025-02-10.
+//
+
+import UIKit
+
+class TreeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
